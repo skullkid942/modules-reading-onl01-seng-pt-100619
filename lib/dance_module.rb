5ac6a87ae514596970f93be085acf773bc581b47
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-14 15:23:21 +0000

    Automatically backed up by Learn

---
+++ lib/dance_module.rb
@@ -0,0 +1,17 @@
+module Dance 
+  def twirl
+    "I'm twirling!"
+  end 
+  
+  def jump
+    "Look how high I'm jumping!"
+  end 
+  
+  def pirouette
+    "I'm doinga pirouette"
+  end 
+  
+  def take_a_bow
+    
+  
+end 
