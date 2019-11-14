d1e380b973a0d7c19dddea4bbea76f05f1ae6cc0
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-14 15:29:55 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,8 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer
-  include Dance 
+  include Dance
+  extend MetaDancing 
   
   attr_accessor :name 
   
